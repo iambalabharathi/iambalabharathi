9ff4f67e9742e5ef9325b9072d662b910775ffb3
Balabharathi Jayaraman <balabharathi.jayaraman@gmail.com>
2021-09-25 23:21:10 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout title="About Page">
+      <p>Senior Automation Developer with 7+ years of experience.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
